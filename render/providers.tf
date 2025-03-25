91228404c1b1dd8e9af5b6f5f7f945a13be720c9
HiC-BGOD <Blane3512@gmail.com>
2025-03-24 20:22:28 -0400

    Terraform done

---
+++ render/providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    render = {
+      source = "render-oss/render"
+      version = "1.3.2"
+    }
+  }
+}
+
+provider "render" {
+  # Configuration options
+  owner_id = usr-cnc01an109ks73d2htb0
+  api_key = rnd_0nobltRK68sUJ1GQSC25ugU7Ddoh
+
+
+
+}
